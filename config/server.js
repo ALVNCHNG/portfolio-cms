7d7ef3d344f16ed850fcfa511731a0231b182b46
ALVNCHNG <alvinching.official@gmail.com>
2021-05-23 00:06:15 +0800

    Initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '15c01bc9a84b398293ed937f4bafe9a6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '17d82da9-327e-4011-bebe-7b3021ff6b7e'
+};
